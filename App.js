3794b2cd910b87c41a722e21586d516c7fc1761f
Nicklas Stellberg <nicklas.stellberg@hotmail.com>
2023-01-16 21:56:51 +0200

    calculating

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import {useState} from 'react';
+import React, { useEffect } from 'react';
 
 export default function App() {
+  const [number1, setNumber1] = React.useState(0);
+  const [number2, setNumber2] = React.useState(0);
+  const [result, setResult] = React.useState('');
+
+  const add = () =>{
+    if (isNaN(number1) || isNaN(number2)) {
+      setResult(0);
+    }
+    else setResult(parseInt(number1) + parseInt(number2));
+  }
+
+  const subtract = () =>{
+    if (isNaN(number1) || isNaN(number2)) {
+      setResult(0);
+    }
+    else setResult(parseInt(number1) - parseInt(number2));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{flex:1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
+      <Text>Result: {result}</Text>
+      <TextInput
+      style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        value={number1}
+        onChangeText={text => setNumber1(text)}
+        keyboardType = 'number-pad'
+      />
+      <TextInput
+      style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        value={number2}
+        onChangeText={text => setNumber2(text)}
+        keyboardType = 'number-pad'
+      />
+      </View>
+      <View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+        <Button onPress={add} title="+" />
+        <Button onPress={subtract} title="-" />
+      </View>
     </View>
   );
 }
